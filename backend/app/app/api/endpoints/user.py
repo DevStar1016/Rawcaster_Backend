b45a3a7fdfe56abf45feffefda94bee95dea254a
dt-surya <surya.durai@team.themaestro.in>
2023-03-22 18:30:37 +0530

    Request 1 to 25 form data

--- backend/app/app/api/endpoints/user.py
+++ backend/app/app/api/endpoints/user.py
@@ -7,9 +7,231 @@ from app.utils import common_date,file_storage,paginate,pagination
 from app.api import deps
 from sqlalchemy.orm import Session
 from app.core.security import settings
-from datetime import datetime
+from datetime import datetime,date
 
 router = APIRouter()
+
+
+# 1 Signup User
+@router.post("/signup")
+async def signup(db:Session=Depends(deps.get_db),signup_type:int=Form(...,description="1-Email,2-Phone Number",ge=1,le=2),first_name:str=Form(...,max_length=100),
+                    last_name:str=Form(None,max_length=100),display_name:str=Form(None,max_length=100),gender:int=Form(None,ge=1,le=2,description="1-male,2-female"),
+                    dob:date=Form(None),email_id:str=Form(None,max_length=100),country_code:int=Form(None),country_id:int=Form(None),
+                    mobile_no:int=Form(None),password:str=Form(...),confirm_password:str=Form(...),geo_location:str=Form(None),
+                    latitude:int=Form(None),longitude:int=Form(None),ref_id:str=Form(None),auth_code:str=Form(...,description="SALT + email_id"),
+                    device_id:str=Form(None),push_id:str=Form(None),device_type:int=Form(None),auth_code1:str=Form(...,description="SALT +username"),
+                    voip_token:str=Form(None),app_type:int=Form(...,description="1-Android,2-IOS",ge=1,le=2)):
+
+    return "done"
+
+
+# 2 - Signup Verification by OTP
+@router.post("/signupverify")
+async def signupverify(db:Session=Depends(deps.get_db),auth_code:str=Form(...,description="SALT + otp_ref_id"),otp_ref_id:str=Form(...,description="From service no. 1"),otp:int=Form(...)):
+    return "done"          
+
+
+# 3 - Resend OTP
+
+@router.post("/resendotp")
+async def resendotp(db:Session=Depends(deps.get_db),auth_code:str=Form(...,description="SALT + otp_ref_id"),otp_ref_id:int=Form(None),token:str=Form(...),otp_flag:str=Form(None)):
+    return "done"
+
+
+
+# 4 - Login
+@router.post("/login")
+async def login(db:Session=Depends(deps.get_db),auth_code:str=Form(...,description="SALT + otp_ref_id"),username:str=Form(...,description="Email ID"),
+                    password:str=Form(...),device_id:str=Form(None),push_id:str=Form(None),device_type:int=Form(None),voip_token:str=Form(None),
+                    app_type:int=Form(None,description="1-> Android, 2-> IOS",ge=1,le=2)):
+    return "done"
+
+
+
+
+
+# 5 - Logout
+@router.post("/logout")
+async def logout(db:Session=Depends(deps.get_db),token:str=Form(...)):
+    return "done"
+
+
+
+
+# 6 - Forgot Password
+@router.post("/forgotpassword")
+async def forgotpassword(db:Session=Depends(deps.get_db),username:str=Form(...,description="Email ID / Mobile Number"),auth_code:str=Form(...,description="SALT + username")):
+    return "done"
+
+
+
+
+
+# 7 - Verify OTP and Reset Password
+@router.post("/verifyotpandresetpassword")
+async def verifyotpandresetpassword(db:Session=Depends(deps.get_db),otp_ref_id:int=Form(...),otp:int=Form(...),
+                                    new_password:str=Form(...),confirm_password:str=Form(...),device_id:str=Form(None),
+                                    push_id:str=Form(None,description="FCM  or APNS"),device_type:int=Form(None),
+                                    auth_code:str=Form(...,description="SALT + otp_ref_id")):
+    return "done"
+
+
+
+
+# 8 - Change Password
+@router.post("/changepassword")
+async def changepassword(db:Session=Depends(deps.get_db),token:str=Form(...),old_password:int=Form(...),new_password:int=Form(...),confirm_password:int=Form(...),auth_code:int=Form(...,description="SALT + token")):
+    return "done"
+
+
+# 9 - Get country list
+@router.post("/getcountylist")
+async def getcountylist(db:Session=Depends(deps.get_db)):
+    return "done"
+
+
+
+
+# 10 - Contact us
+@router.post("/contactus")
+async def contactus(db:Session=Depends(deps.get_db),name:str=Form(...),email_id:str=Form(...),subject:str=Form(...),message:str=Form(...),
+                    auth_code:str=Form(...,description="SALT + email_id")):
+    return "done"
+
+
+
+# 11 - Get My Profile
+@router.post("/getmyprofile")
+async def getmyprofile(db:Session=Depends(deps.get_db),token:str=Form(...),auth_code:str=Form(...,description="SALT + token")):
+    return "done"
+
+
+# 12. Update My Profile
+@router.post("/updatemyprofile")
+async def updatemyprofile(db:Session=Depends(deps.get_db),token:str=Form(...),name:str=Form(...),first_name:str=Form(...),last_name:str=Form(None),
+                          gender:int=Form(None,description="0->Transgender,1->Male,2->Female",ge=0,le=2),dob:date=Form(None),
+                          email_id:str=Form(...),website:str=Form(None),country_code:int=Form(None),country_id:int=Form(None),
+                          mobile_no:int=Form(None),profile_image:UploadFile=File(None),cover_image:UploadFile=File(None),
+                          auth_code:str=Form(...,description="SALT + token + name"),geo_location:str=Form(None),latitude:str=Form(None),
+                          longitude:str=Form(None)):
+    return "done"
+
+
+# 13 Search Rawcaster Users (for friends)
+@router.post("/searchrawcasterusers")
+async def searchrawcasterusers(db:Session=Depends(deps.get_db),token:str=Form(...,description="Any name, email"),auth_code:str=Form(...,description="SALT + token"),
+                               search_for:int=Form(None,description="0-Pending,1-Accepted,2-Rejected,3-Blocked",ge=0,le=3),page_number:int=Form(None)):
+                         
+    return "done"
+
+
+
+# 14 Invite to Rawcaster
+@router.post("/invitetorawcaster")
+async def invitetorawcaster(db:Session=Depends(deps.get_db),token:str=Form(...),email_id:list=Form(...,description="email ids"),
+                               auth_code:int=Form(None,description="SALT + token")):
+                         
+    return "done"
+
+
+
+
+# 15 Invite to Rawcaster
+@router.post("/invitetorawcaster")
+async def invitetorawcaster(db:Session=Depends(deps.get_db),token:str=Form(...),email_id:list=Form(...,description="email ids"),
+                               auth_code:int=Form(None,description="SALT + token")):
+                         
+    return "done"
+
+
+
+# 16 List all friend requests (all requests sent to this users from others)
+@router.post("/listallfriendrequests")
+async def listallfriendrequests(db:Session=Depends(deps.get_db),token:str=Form(...)):
+                         
+    return "done"
+
+
+
+# 17 Respond to friend request received from others
+@router.post("/respondtofriendrequests")
+async def respondtofriendrequests(db:Session=Depends(deps.get_db),token:str=Form(...),friend_request_id:int=Form(...),notification_id:int=Form(...),
+                                  response:int=Form(...,description="1-Accept,2-Reject,3-Block",ge=1,le=3)):
+                         
+    return "done"
+
+
+
+
+# 18 List all Friend Groups
+@router.post("/listallfriendgroups")
+async def listallfriendgroups(db:Session=Depends(deps.get_db),token:str=Form(...),search_key:str=Form(None),page_number:int=Form(default=1)):
+                         
+    return "done"
+
+
+# 19 List all Friends
+@router.post("/listallfriends")
+async def listallfriends(db:Session=Depends(deps.get_db),token:str=Form(...),search_key:str=Form(None),group_ids:str=Form(None,description="Like ['12','13','14']"),
+                         nongrouped:int=Form(None,description="Send 1",ge=1,le=1),friends_count:int=Form(None),allfriends:int=Form(None,description="send 1",ge=1,le=1),
+                         page_number:int=Form(None,description="send 1 for initial request")):
+                         
+    return "done"
+
+
+
+
+# 20 Add Friend Group
+@router.post("/addfriendgroup")
+async def addfriendgroup(db:Session=Depends(deps.get_db),token:str=Form(...),group_name:str=Form(...),group_members:str=Form(None,description=" User ids Like ['12','13','14']"),
+                         group_icon:UploadFile=File(None)):
+                         
+    return "done"
+
+
+
+# 21 Edit Friend Group
+@router.post("/editfriendgroup")
+async def editfriendgroup(db:Session=Depends(deps.get_db),token:str=Form(...),group_name:str=Form(...),group_id:int=Form(...),
+                         group_icon:UploadFile=File(None)):
+                         
+    return "done"
+
+
+
+
+# 22 Add Friends to Group
+@router.post("/addfriendstogroup")
+async def addfriendstogroup(db:Session=Depends(deps.get_db),token:str=Form(...),group_members:str=Form(None,description=" User ids Like ['12','13','14']"),group_id:int=Form(...)):
+                         
+    return "done"
+
+
+
+
+# 23 Remove Friends from Group
+@router.post("/removefriendsfromgroup")
+async def removefriendsfromgroup(db:Session=Depends(deps.get_db),token:str=Form(...),group_members:str=Form(None,description=" User ids Like ['12','13','14']"),group_id:int=Form(...)):
+                         
+    return "done"
+
+
+
+# 24. Delete Friend Group
+@router.post("/deletefriendgroup")
+async def deletefriendgroup(db:Session=Depends(deps.get_db),token:str=Form(...),group_id:int=Form(...)):
+                         
+    return "done"
+
+
+
+
+# 25. Add Nuggets
+@router.post("/addnuggets")
+async def addnuggets(db:Session=Depends(deps.get_db),token:str=Form(...),content:str=Form(...),share_type:int=Form(None),share_with:str=Form(...,description='friends":[1,2,3],"groups":[1,2,3]}'),
+                     nuggets_media:UploadFile=File(...),poll_option:str=Form(None),poll_duration:str=Form(None)):
+                         
+    return "done"
 
 
 # # Change password 
