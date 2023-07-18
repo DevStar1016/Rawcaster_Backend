6648bda2a946886a40f3006502979b34e3ff8ae9
surya_maestro <surya.durai@team.themaestro.in>
2023-07-18 12:23:25 +0530

    using aioredis module to connect elastic cluster

--- backend/app/app/api/endpoints/elastic.py
+++ backend/app/app/api/endpoints/elastic.py
@@ -4,15 +4,35 @@ from fastapi import APIRouter, Depends, Form, File, UploadFile
 
 
 router = APIRouter()
-cache = redis.Redis(host='raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com', port=6379, db=0)
 
-@router.get("/test11")
-def get_data():
-    print('test')
-    value = cache.get("key")
-    print(value)
+
+# cache = redis.Redis(host='raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com', port=6379, db=0)
+
+# @router.get("/test11")
+# def get_data():
+#     print('test')
+#     value = cache.get("key")
+#     print(value)
+#     if value is None:
+#         # Value not found in cache, fetch from the database
+#         value = fetch_data_from_database()
+#         cache.set("key", value)
+#     return {"data": value}
+
+import aioredis
+
+async def connect_to_redis():
+    redis_pool = await aioredis.create_redis_pool('redis://your-elasticache-endpoint:your-elasticache-port')
+
+@router.on_event("startup")
+async def startup_event():
+    await connect_to_redis()
+    
+
+@router.get('/cache/{key}')
+async def get_value_from_cache(key: str):
+    value = await redis_pool.get(key)
     if value is None:
-        # Value not found in cache, fetch from the database
-        value = fetch_data_from_database()
-        cache.set("key", value)
-    return {"data": value}
+        return {'message': 'Key not found'}
+    else:
+        return {'key': key, 'value': value.decode('utf-8')}
