d62f0e09cd2dc2c1df5023ff50b8ecb2e003c246
surya_maestro <surya.durai@team.themaestro.in>
2023-07-19 14:01:14 +0530

    elastic connection

--- backend/app/app/api/endpoints/elastic.py
+++ backend/app/app/api/endpoints/elastic.py
@@ -21,25 +21,3 @@ def elastic_connection():
         
     
     
-# Replace 'your_elasticache_endpoint' and 'your_elasticache_port' with your actual ElastiCache endpoint and port
-elasticache_endpoint = 'raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com'
-elasticache_port = 6379
-
-# Create a Redis client
-redis_client = redis.StrictRedis(host=elasticache_endpoint, port=elasticache_port, decode_responses=True)
-
-
-
-@router.post("/read_root")
-def read_root():
-    # Write data to Redis
-    redis_client.set("key", "value")
-
-    # Read data from Redis
-    value = redis_client.get("key")
-    return {"message": f"Value in Redis: {value}"}
-
-
-@router.post("/read_root1")
-def read_root1():
-    return "Success"
