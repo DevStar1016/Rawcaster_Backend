3bc0f77c844d8c54d54cf2ea229513bc9ed30aa7
surya_maestro <surya.durai@team.themaestro.in>
2023-07-19 10:04:00 +0530

    elastic connection

--- backend/app/app/api/endpoints/elastic.py
+++ backend/app/app/api/endpoints/elastic.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends, Form, File, UploadFile
 router = APIRouter()
 cache = redis.Redis(host='raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com', port=6379, db=0)
 
-@router.get("/elastic_connection")
+@router.post("/elastic_connection")
 def elastic_connection():
     from redis import Redis
 
@@ -19,3 +19,28 @@ def elastic_connection():
         print('I am Redis. Try me. I can remember things, only for a short time though :)')
     except Exception as e:
         print('EXCEPTION: host could not be accessed ---> ', repr(e))
+        
+    
+    
+# Replace 'your_elasticache_endpoint' and 'your_elasticache_port' with your actual ElastiCache endpoint and port
+elasticache_endpoint = 'raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com'
+elasticache_port = 6379
+
+# Create a Redis client
+redis_client = redis.StrictRedis(host=elasticache_endpoint, port=elasticache_port, decode_responses=True)
+
+
+
+@router.post("/read_root")
+def read_root():
+    # Write data to Redis
+    redis_client.set("key", "value")
+
+    # Read data from Redis
+    value = redis_client.get("key")
+    return {"message": f"Value in Redis: {value}"}
+
+
+@router.post("/read_root1")
+def read_root1():
+    return "Success"
