b4f0b6bcb461ae8df505c8e52b16fc8007e860ff
surya_maestro <surya.durai@team.themaestro.in>
2023-07-19 17:08:26 +0530

    db host change3

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -26,10 +26,10 @@ base_url_segment = "/rawcaster"
 base_upload_folder = "local_uploads"
 
 # Elastic Cache
-# data_base ='mysql+pymysql://Admin:Rawcaster2240422@raew7no6l92n8p4-001.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com:6379/rawcasterdev'
+data_base ='Admin:Rawcaster2240422@replica.raew7no6l92n8p4.rljwzo.use1.cache.amazonaws.com:6379/rawcasterdev'
 
 # Dev
-data_base ='mysql+pymysql://Admin:Rawcaster2240422@rawcasterdev.c3xecvdvxfvf.us-east-1.rds.amazonaws.com/rawcasterdev'
+# data_base ='mysql+pymysql://Admin:Rawcaster2240422@rawcasterdev.c3xecvdvxfvf.us-east-1.rds.amazonaws.com/rawcasterdev'
 
 # Local
 # data_base = "mysql+pymysql://maemysqluser:%s@cbe.themaestro.in/rawcaster" % quote(
