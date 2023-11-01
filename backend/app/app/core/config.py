2178a590a7178218b8854a8272eadd11f43ae300
bharath-selvaraj <98140438+bharath-selvaraj@users.noreply.github.com>
2023-11-01 10:32:16 +0530

    Update config.py

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -45,10 +45,10 @@ base_url_segment = "/rawcaster"
 base_upload_folder = "local_uploads"
 
 # Dev DB Connection Details
-#data_base =credentials['db_connection']
+data_base =credentials['db_connection']
 
 # Local
-data_base = "mysql+pymysql://maemysqluser:%s@cbe.themaestro.in/rawcaster" % quote("MaeNewMysql2@2@")
+#data_base = "mysql+pymysql://maemysqluser:%s@cbe.themaestro.in/rawcaster" % quote("MaeNewMysql2@2@")
 
 api_doc_path = "/docs"
 
