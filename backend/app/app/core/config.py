89ee38a96d228db8d96e5872ed3eba7b4f17cc9e
dt-surya <surya.durai@team.themaestro.in>
2023-11-30 11:51:44 +0530

    change production database4

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -49,7 +49,8 @@ base_upload_folder = "local_uploads"
 
 # Production Database
 
-data_base = "mysql+pymysql://mdbuser:M@estr0123!@44.239.190.244/rawcaster_production"
+data_base ='mysql+pymysql://mdbuser:%s@44.239.190.244/rawcaster_production' % quote('M@estr0123!')
+
 
 api_doc_path = "/docs"
 
